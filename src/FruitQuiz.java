3cf5034e6e1135cc8bc0a8b622cb01b3d56f8110
league <league@wts19.attlocal.net>
2018-04-23 19:57:06 -0700

    FruitQuiz, HappyPet

--- src/FruitQuiz.java
+++ src/FruitQuiz.java
@@ -1,6 +1,6 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
 
 import java.applet.AudioClip;
 import java.awt.event.KeyAdapter;
@@ -13,8 +13,10 @@ import javax.swing.JLabel;
 public class FruitQuiz extends KeyAdapter {
 
 	void makeQuestions() {
-		question1 = new JLabel("<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
-		// 11. Make another question called "question2".  Use question1 above as a guide.
+		question2 = new JLabel(
+				"<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
+		// 11. Make another question called "question2". Use question1 above as a guide.
+		question1 = new JLabel("<html>What is the best pet <br> A: Dog <br> B: Cat <br> C: Fish");
 	}
 
 	@Override
@@ -25,42 +27,52 @@ public class FruitQuiz extends KeyAdapter {
 		// 2. Make 3 int variables that hold the key codes for A, b, and C
 		int a = 65;
 		int b = 66;
-		int c = 67; 
-		
-		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
-		
+		int c = 67;
+
+		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The
+		// questions must be in reverse order from top to bottom to work properly
+
 		// 12. If question2 is showing,
-			
-			// 13. check if it is right or wrong like you did for question1
-		
-			
+		if (question2.isShowing()) {
+			if (keyCode == c) {
+				correct();
+
+			}
+		}
+		// 13. check if it is right or wrong like you did for question1
+
 		if (question1.isShowing()) {
 			// 3. If they selected the right fruit, do steps 4 and 7
-			if (keyCode == c) {
+			if (keyCode == a) {
 				// 4. Call the correct() method
-				
+				correct();
 				// 7. Use the nextQuestion() method to go to question2
-			
-			
+				nextQuestion(question2);
+			}
+
 			// 8. else (if they touched something else)
-				
-				// 9. Call the incorrect() method
-		
+			else {
+				incorrect();
+			}
+			// 9. Call the incorrect() method
+
 		}
 
 	}
 
 	private void correct() {
+		playSound("correct.wav");
 		// 5. Find a sound for when they get a question right, and drag it into
-		// the default package. It must be a .wav file. 
+		// the default package. It must be a .wav file.
 		// There are lots on freesound.org
 		// 6. Use the playSound method to play your sound
-
 
 	}
 
 	private void incorrect() {
-		// 10. Find a sound for wrong answers and put it in the default package. Use the playSound method to play it.
+		playSound("wrong.wav");
+		// 10. Find a sound for wrong answers and put it in the default package. Use the
+		// playSound method to play it.
 
 	}
 
@@ -97,4 +109,3 @@ public class FruitQuiz extends KeyAdapter {
 	JLabel question1 = new JLabel(), question2 = new JLabel(), question3 = new JLabel(), question4 = new JLabel(),
 			question5;
 }
-
--- src/HappyPet.java
+++ src/HappyPet.java
@@ -4,23 +4,38 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
+	static String answer;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+ answer = JOptionPane.showInputDialog("What kind of pet do you want to buy?"); 
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
-
+			int task = JOptionPane.showOptionDialog(null, "What would you do to make your pet happy?" , "Pet Care", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Walk", "Feed", "Play" }, null);
+System.out.println(task);
 			// 5. Use user input to call the appropriate method created in step 4.
-
-			// 6. If you determine the happiness level is large enough, tell the
+//feed equals 1
+//walk equals 2
+//play equals 		// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
+	}
+	static void Walk() {
+		happinessLevel += 5;
+		JOptionPane.showMessageDialog(null, answer + " is hungry from the walk.");
+		
+	}
+	static void Feed() {
+		happinessLevel += 10;
+		JOptionPane.showMessageDialog(null, answer + " is full.");
+	}
+	static void Play() {
+		happinessLevel += 15;
+		JOptionPane.showMessageDialog(null, answer + " is tired.");
 	}
 
 	// 4. Create methods to handle each of your user selections.
